0b6075c0a41454e52ab3e4732e711fdf62b583a9
Ryan Suhartanto <suhartanto@kekkon.nexus>
2026-07-20 13:37:11 +0700

    refactor: simplify password omitting

--- model/user.go
+++ model/user.go
@@ -1,19 +1,11 @@
 package model
 
-import "encoding/json"
+type Password string
+
+func (Password) IsZero() bool { return true }
 
 type User struct {
-	Name     string `json:"name" binding:"required"`
-	Email    string `json:"email" binding:"required"`
-	Password string `json:"password" binding:"required"`
-}
-
-func (u User) MarshalJSON() ([]byte, error) {
-	type Alias User
-	return json.Marshal(&struct {
-		Password string `json:"password,omitempty"`
-		*Alias
-	}{
-		Alias: (*Alias)(&u),
-	})
+	Name     string   `json:"name" binding:"required"`
+	Email    string   `json:"email" binding:"required"`
+	Password Password `json:"password,omitzero" binding:"required"`
 }
